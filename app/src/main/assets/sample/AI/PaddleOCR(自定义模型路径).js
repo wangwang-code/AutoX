654ab17d7229f8dcbde3bef55d3db5294b8b10b3
LZX284 <105829296+LZX284@users.noreply.github.com>
2022-07-23 09:18:23 +0800

    升级OpenCV到4.6.0；升级PaddleOCR到2.5.0(兼容V2版模型)

--- app/src/main/assets/sample/AI/PaddleOCR(自定义模型路径).js
+++ app/src/main/assets/sample/AI/PaddleOCR(自定义模型路径).js
@@ -1,11 +1,8 @@
 let img = images.read("./0.jpg")
 let cpuThreadNum = 4
 // 新增：自定义模型路径(必须是完整路径)
-// 使用时请将自定义路径 myModelPath 改为拥有读写权限的目录，否则可能引起程序崩溃
-// 目录中必须包含三个模型文件： ch_ppocr_mobile_v2.0_cls_opt.nb ch_ppocr_mobile_v2.0_det_opt.nb ch_ppocr_mobile_v2.0_rec_opt.nb，否则将改用内置的模型。
-let myModelPath = "/sdcard/Android/data/org.autojs.autojs.dev/models/ocr_v2_for_cpu";
-// 如果打包了自定义模型路径，则路径为"models/自定义的ocr模型文件夹名字"
-//let myModelPath = "models/my_ocr_models_dir";
+// 使用时请将自定义路径 myModelPath 改为拥有读写权限的目录，并在目录里放上三个必需的模型文件： cls_opt.nb det_opt.nb rec_opt.nb，否则可能引起程序崩溃。
+let myModelPath = "/sdcard/models/ocr_v2_for_cpu";
 let start = new Date()
 // 识别图片中的文字，返回完整识别信息（兼容百度OCR格式）。
 let result = paddle.ocr(img, cpuThreadNum, myModelPath)
@@ -21,3 +18,4 @@ toastLog("文本识别信息: " + JSON.stringify(stringList))
 img.recycle()
 // 释放native内存，非必要，供万一出现内存泄露时使用
 // paddle.release()
+
--- autojs/src/main/java/com/stardust/autojs/runtime/api/Paddle.java
+++ autojs/src/main/java/com/stardust/autojs/runtime/api/Paddle.java
@@ -15,15 +15,24 @@ import java.util.List;
 public class Paddle {
     private Predictor mPredictor = new Predictor();
 
-    public void initOcr(Context context, int cpuThreadNum, boolean useSlim) {
-        mPredictor.initOcr(context, cpuThreadNum, useSlim);
+    public boolean initOcr(Context context) {
+        return mPredictor.init(context);
     }
 
-    public boolean initOcr(Context context, int cpuThreadNum, String myModelPath) {
-        return mPredictor.initOcr(context, cpuThreadNum, myModelPath);
+    public boolean initOcr(Context context, int cpuThreadNum) {
+        return mPredictor.init(context, cpuThreadNum);
     }
 
-    public List<OcrResult> ocr(ImageWrapper image, int cpuThreadNum, boolean useSlim) {
+    public boolean initOcr(Context context, int cpuThreadNum, Boolean useSlim, Boolean useOpencl) {
+        return mPredictor.init(context, cpuThreadNum, useSlim, useOpencl);
+    }
+
+    public boolean initOcr(Context context, int cpuThreadNum, String myModelPath, Boolean useOpencl) {
+        return mPredictor.init(context, cpuThreadNum, myModelPath, useOpencl);
+    }
+
+    public List<OcrResult> ocr(ImageWrapper image, int cpuThreadNum, String myModelPath, Boolean useOpencl,
+                               int detLongSize, float scoreThreshold) {
         if (image == null) {
             return Collections.emptyList();
         }
@@ -32,62 +41,106 @@ public class Paddle {
             return Collections.emptyList();
         }
         if (!mPredictor.isLoaded()) {
-            initOcr(GlobalAppContext.get(), cpuThreadNum, useSlim);
+            mPredictor.init(GlobalAppContext.get(), cpuThreadNum, myModelPath, useOpencl, detLongSize, scoreThreshold);
         }
-        return mPredictor.runOcr(bitmap, 4, true);
+        return mPredictor.ocr(bitmap);
+    }
+
+    public List<OcrResult> ocr(ImageWrapper image, int cpuThreadNum, Boolean useSlim, Boolean useOpencl,
+                               int detLongSize, float scoreThreshold) {
+        if (image == null) {
+            return Collections.emptyList();
+        }
+        Bitmap bitmap = image.getBitmap();
+        if (bitmap == null || bitmap.isRecycled()) {
+            return Collections.emptyList();
+        }
+        if (!mPredictor.isLoaded()) {
+            mPredictor.init(GlobalAppContext.get(), cpuThreadNum, useSlim, useOpencl, detLongSize, scoreThreshold);
+        }
+        return mPredictor.ocr(bitmap);
+    }
+
+    public List<OcrResult> ocr(ImageWrapper image, int cpuThreadNum, Boolean useSlim, Boolean useOpencl) {
+        if (image == null) {
+            return Collections.emptyList();
+        }
+        Bitmap bitmap = image.getBitmap();
+        if (bitmap == null || bitmap.isRecycled()) {
+            return Collections.emptyList();
+        }
+        if (!mPredictor.isLoaded()) {
+            mPredictor.init(GlobalAppContext.get(), cpuThreadNum, useSlim, useOpencl);
+        }
+        return mPredictor.ocr(bitmap);
+    }
+
+    public List<OcrResult> ocr(ImageWrapper image, int cpuThreadNum, String myModelPath, Boolean useOpencl) {
+        if (image == null) {
+            return Collections.emptyList();
+        }
+        Bitmap bitmap = image.getBitmap();
+        if (bitmap == null || bitmap.isRecycled()) {
+            return Collections.emptyList();
+        }
+        if (!mPredictor.isLoaded()) {
+            mPredictor.init(GlobalAppContext.get(), cpuThreadNum, myModelPath, useOpencl);
+        }
+        return mPredictor.ocr(bitmap);
     }
 
     public List<OcrResult> ocr(ImageWrapper image, int cpuThreadNum, String myModelPath) {
-        if (image == null) {
-            return Collections.emptyList();
-        }
-        Bitmap bitmap = image.getBitmap();
-        if (bitmap == null || bitmap.isRecycled()) {
-            return Collections.emptyList();
-        }
-        if (!mPredictor.isLoaded()) {
-            initOcr(GlobalAppContext.get(), cpuThreadNum, myModelPath);
-        }
-        return mPredictor.runOcr(bitmap, 4, true);
-    }
-
-    public List<OcrResult> ocr(ImageWrapper image, int cpuThreadNum) {
-        return ocr(image, cpuThreadNum, true);
+        return ocr(image, cpuThreadNum, myModelPath, false);
     }
 
     public List<OcrResult> ocr(ImageWrapper image) {
-        return ocr(image, 4, true);
+        return ocr(image, 4, true, false);
     }
 
-    public String[] ocrText(ImageWrapper image, int cpuThreadNum, boolean useSlim) {
-        List<OcrResult> wordsResult = ocr(image, cpuThreadNum, useSlim);
-        return sortResult(wordsResult);
-    }
-
-    public String[] ocrText(ImageWrapper image, int cpuThreadNum, String myModelPath) {
-        List<OcrResult> wordsResult = ocr(image, cpuThreadNum, myModelPath);
-        return sortResult(wordsResult);
-    }
-
-    public String[] ocrText(ImageWrapper image, int cpuThreadNum) {
-        return ocrText(image, cpuThreadNum, true);
-    }
-
-    public String[] ocrText(ImageWrapper image) {
-        return ocrText(image, 4, true);
-    }
-
-    public void release() {
-        mPredictor.releaseOcr();
-    }
-
-    private String[] sortResult(List<OcrResult> src) {
-        String[] outputResult = new String[src.size()];
-        for (int i = 0; i < src.size(); i++) {
-            outputResult[i] = src.get(i).words;
+    public String[] ocrText(ImageWrapper image, int cpuThreadNum, Boolean useSlim, Boolean useOpencl) {
+        if (!mPredictor.isLoaded()) {
+            mPredictor.init(GlobalAppContext.get(), cpuThreadNum, useSlim, useOpencl);
+        }
+        List<OcrResult> words_result = ocr(image, cpuThreadNum, useSlim, useOpencl);
+        String[] outputResult = new String[words_result.size()];
+        for (int i = 0; i < words_result.size(); i++) {
+            outputResult[i] = words_result.get(i).words;
             Log.i("outputResult", outputResult[i].toString()); // show LOG in Logcat panel
         }
         return outputResult;
     }
 
+    public String[] ocrText(ImageWrapper image, int cpuThreadNum, String myModelPath, Boolean useOpencl) {
+        List<OcrResult> words_result = ocr(image, cpuThreadNum, myModelPath, useOpencl);
+        String[] outputResult = new String[words_result.size()];
+        for (int i = 0; i < words_result.size(); i++) {
+            outputResult[i] = words_result.get(i).words;
+            Log.i("outputResult", outputResult[i].toString()); // show LOG in Logcat panel
+        }
+        return outputResult;
+    }
+
+    public String[] ocrText(ImageWrapper image, int cpuThreadNum, Boolean useSlim) {
+        return ocrText(image, cpuThreadNum, useSlim, false);
+    }
+
+    public String[] ocrText(ImageWrapper image, int cpuThreadNum, String myModelPath) {
+        return ocrText(image, cpuThreadNum, myModelPath, false);
+    }
+
+    public String[] ocrText(ImageWrapper image) {
+        return ocrText(image, 4, true, false);
+    }
+
+    public String[] ocrText(ImageWrapper image, int cpuThreadNum) {
+        return ocrText(image, cpuThreadNum, true, false);
+    }
+
+    public void releaseOcr() {
+        mPredictor.releaseModel();
+    }
+
+    public void release() {
+        mPredictor.releaseModel();
+    }
 }
