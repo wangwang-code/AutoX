26645d0a8561e38a34f3f173e7073fe593d73de1
wangwang-code <64469117+wangwang-code@users.noreply.github.com>
2023-04-22 21:09:43 +0800

    模板导入opencc

--- inrt/build.gradle.kts
+++ inrt/build.gradle.kts
@@ -192,6 +192,10 @@ fun copyTemplateToAPP(isDebug: Boolean) {
 }
 
 dependencies {
+    // opencc
+    implementation("com.github.qichuan:android-opencc:1.2.0")
+    implementation("androidx.appcompat:appcompat:1.3.0")
+
     implementation("androidx.activity:activity-ktx:1.5.0")
     implementation("androidx.appcompat:appcompat:1.4.2")
     implementation("com.google.android.material:material:1.6.1")
